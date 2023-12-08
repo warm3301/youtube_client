bf39247b58215335d6140d90a7dd377428a30a8d
warm3301 <warm3301@gmail.com>
2023-12-08 12:20:11 +0300

    part downloading and auto compile audio and video

--- youtube_client/downloaders/base_downloader.py
+++ youtube_client/downloaders/base_downloader.py
@@ -12,31 +12,32 @@ class VideoDownloader:
         skip_existing: bool = True
         ):
         self.stream = stream
+        self.def_stream = self.stream[0] if isinstance(self.stream,tuple) else self.stream
         self.default_title = default_title
         self.on_complete = on_complete
         self.file_path = self.get_file_path(output_path,ext_in_path)
         self.skip = skip_existing and self.exists_at_path(self.file_path)
 
 
-
     @property
     def default_filename(self) -> str:#TODO update helpers.generate_filename for base includig directory and del param directory 
         #TODO and add param postfix
         #TODO separator if not added symbols to base
+
         name=self.default_title
-        if self.stream.title:
-            name= self.stream.title
+        if self.def_stream.title:
+            name= self.def_stream.title
         filename = safe_filename(name)
-        return f"{filename}.{self.stream.ext}"
+        return f"{filename}.{self.def_stream.ext}"
     def get_file_path(self,file_path:Optional[str],ext_contain:bool=False)->str:
         path = file_path
         if path == None:
             return self.default_filename
         if not ext_contain:
-            path += f".{self.stream.ext}"
+            path += f".{self.def_stream.ext}"
         return path
     def exists_at_path(self, file_path: str) -> bool:
-        return os.path.isfile(file_path) and os.path.getsize(file_path) == self.stream.filesize
+        return os.path.isfile(file_path) #and os.path.getsize(file_path) == self.stream.filesize
     def _on_complete(self,file_path):
         if self.on_complete:
             self.on_complete(self,file_path)
--- youtube_client/downloaders/ffpmeg_downloader.py
+++ youtube_client/downloaders/ffpmeg_downloader.py
@@ -1,20 +1,45 @@
-from typing import Optional,Callable,Any
+# https://github.com/jonghwanhyeon/python-ffmpeg
+from typing import Optional,Callable,Any,Union,Tuple
 from ffmpeg import FFmpeg, Progress
 from .base_downloader import VideoDownloader
 from ..streams import Stream
+
 class FFMpegDownloader(VideoDownloader):
     def __init__(self,
-    stream:Stream,
+    streams:Union[Stream,Tuple[Stream]],
     output_path:str=None,
     ext_in_path:bool=False,
     skip_existing: bool = True,
+    time_from:str = None,
+    time_to:str = None,
+    lenght:str = None,
     on_complete:Optional[Callable[[Any,str],None]]=None,
     on_progress:Optional[Callable[[Any,"Progress"],None]]=None,
     default_title:str = "Unknown youtube video",
     ):
         self.on_progress = on_progress
-        super().__init__(stream=stream,on_complete=on_complete,default_title=default_title,output_path=output_path,ext_in_path=ext_in_path,skip_existing=skip_existing)
-        self.ffdownloader = (FFmpeg().option("y").input(self.stream.url).output(self.file_path,vcodec="copy",acodec="copy"))
+        super().__init__(stream=streams,on_complete=on_complete,default_title=default_title,output_path=output_path,ext_in_path=ext_in_path,skip_existing=skip_existing)
+        self.ffdownloader = FFmpeg().option("y")
+        params = {}
+        if time_from:
+            params["ss"]=time_from
+        if time_to:
+            params["to"]=time_to
+        if lenght:
+            params["t"]=lenght
+        if isinstance(streams,Stream):
+            self.ffdownloader.input(self.stream.url,params)
+        elif isinstance(streams,tuple):
+            if len(streams) == 1:
+                self.ffdownloader.input(self.stream[0].url,params)
+            elif len(streams) == 2:
+                self.ffdownloader.input(self.stream[0].url,params)
+                self.ffdownloader.input(self.stream[1].url,params)
+            else:
+                raise Exception("streams is not Stream or tuple of streams contains 2 or 1 Stream")
+        else:
+            raise Exception("streams is not Stream or tuple of streams contains 2 or 1 Stream")
+        self.ffdownloader.output(self.file_path,vcodec="copy",acodec="copy")
     
     def download(self):
         if self.skip:
